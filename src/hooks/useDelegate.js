a59fcbfdb4c3a54b60dd63a5c4f5755fb37861ed
JoshdfG <agboolajoshua161@gmail.com>
2024-03-09 15:30:16 +0100

    initial-commit

--- src/hooks/useDelegate.js
+++ src/hooks/useDelegate.js
@@ -1,6 +1,3 @@
-// Import the useCallback hook from React. This prevents unnecessary rerendering by keeping a
-// function in memory until a necessary state change.
-// Import necessary utilities and constants
 import { useCallback } from "react";
 import { isSupportedChain } from "../utils";
 import { isAddress } from "ethers";
@@ -10,22 +7,16 @@ import { getProposalsContract } from "../constants/contracts";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-// Import the necessary methods to access account and provider details from Web3Modal's ethers adapter
 import {
   useWeb3ModalAccount,
   useWeb3ModalProvider,
 } from "@web3modal/ethers/react";
 
-// Define a custom hook `useDelegate` which takes in the decoded user's address as an argument
 const useDelegate = (delegateToAddress) => {
-  // Use the imported hooks to retrieve the chainId from the user's account and the user's wallet provider
   const { chainId } = useWeb3ModalAccount();
   const { walletProvider } = useWeb3ModalProvider();
 
-  // Use useCallback to define a function that will be kept in memory until necessary state changes take place.
-  // In this case, it will delegate votes to another address.
   return useCallback(async () => {
-    // Make sure the active network is supported by the dApp and the delegation address is valid
     if (!isSupportedChain(chainId)) {
       toast.error("Wrong network");
       return;
@@ -35,14 +26,11 @@ const useDelegate = (delegateToAddress) => {
       return;
     }
 
-    // Get the current wallet provider and signer
     const readWriteProvider = getProvider(walletProvider);
     const signer = await readWriteProvider.getSigner();
 
-    // Get the contract with the signer who will make transactions
     const contract = getProposalsContract(signer);
 
-    // Try to delegate the votes to a specific address and print the result of operation
     try {
       const transaction = await contract.delegate(delegateToAddress);
       toast.info("Delegating votes...");
@@ -50,9 +38,9 @@ const useDelegate = (delegateToAddress) => {
       const receipt = await transaction.wait();
 
       if (receipt.status) {
-        toast.success("Successful delegation!");
+        toast.success("Delegation completed successfully");
       } else {
-        toast.error("Failed delegation!");
+        toast.error("OOps! Delegation failed");
       }
     } catch (error) {
       toast.error("Something went wrong");
@@ -60,5 +48,4 @@ const useDelegate = (delegateToAddress) => {
   }, [delegateToAddress, chainId, walletProvider]);
 };
 
-// Export the `useDelegate` hook
 export default useDelegate;
